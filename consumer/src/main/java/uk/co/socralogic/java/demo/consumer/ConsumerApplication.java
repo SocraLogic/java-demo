ee1e02cbdf9d30665b77707063b368d13634d14e
Kevin Timmins <kevin.timmins@socralogic.co.uk>
2018-08-04 21:35:10 +0100

    initial commit

---
+++ consumer/src/main/java/uk/co/socralogic/java/demo/consumer/ConsumerApplication.java
@@ -0,0 +1,12 @@
+package uk.co.socralogic.java.demo.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerApplication.class, args);
+	}
+}
